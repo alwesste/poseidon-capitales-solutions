ad3aa8647537dd9edc77ed8218d302f9ec022d74
Leopold <leopolddag@gmail.com>
2025-08-29 22:34:48 +0200

    Mise a jour du fichier ressource

--- src/main/resources/doc/data.sql
+++ src/main/resources/doc/data.sql
@@ -1,6 +1,5 @@
-
-CREATE TABLE BidList (
-  BidListId tinyint(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS BidList (
+  bidListId INT NOT NULL AUTO_INCREMENT,
   account VARCHAR(30) NOT NULL,
   type VARCHAR(30) NOT NULL,
   bidQuantity DOUBLE,
@@ -22,12 +21,11 @@ CREATE TABLE BidList (
   dealType VARCHAR(125),
   sourceListId VARCHAR(125),
   side VARCHAR(125),
-
   PRIMARY KEY (BidListId)
-)
+);
 
-CREATE TABLE Trade (
-  TradeId tinyint(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Trade (
+  tradeId INT NOT NULL AUTO_INCREMENT,
   account VARCHAR(30) NOT NULL,
   type VARCHAR(30) NOT NULL,
   buyQuantity DOUBLE,
@@ -48,52 +46,51 @@ CREATE TABLE Trade (
   dealType VARCHAR(125),
   sourceListId VARCHAR(125),
   side VARCHAR(125),
-
   PRIMARY KEY (TradeId)
-)
+);
 
-CREATE TABLE CurvePoint (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS CurvePoint (
+  id INT NOT NULL AUTO_INCREMENT,
   CurveId tinyint,
   asOfDate TIMESTAMP,
   term DOUBLE ,
   value DOUBLE ,
   creationDate TIMESTAMP ,
-
   PRIMARY KEY (Id)
-)
+);
 
-CREATE TABLE Rating (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Rating (
+  id INT NOT NULL AUTO_INCREMENT,
   moodysRating VARCHAR(125),
   sandPRating VARCHAR(125),
   fitchRating VARCHAR(125),
   orderNumber tinyint,
-
   PRIMARY KEY (Id)
-)
+);
 
-CREATE TABLE RuleName (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS RuleName (
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(125),
   description VARCHAR(125),
   json VARCHAR(125),
   template VARCHAR(512),
   sqlStr VARCHAR(125),
   sqlPart VARCHAR(125),
-
   PRIMARY KEY (Id)
-)
+);
 
-CREATE TABLE Users (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Users (
+  id INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(125),
   password VARCHAR(125),
   fullname VARCHAR(125),
   role VARCHAR(125),
-
   PRIMARY KEY (Id)
-)
+);
 
-insert into Users(fullname, username, password, role) values("Administrator", "admin", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "ADMIN")
-insert into Users(fullname, username, password, role) values("User", "user", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "USER")
+TRUNCATE TABLE users;
+
+insert into Users(fullname, username, password, role) VALUES
+("Administrator", "admin", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "ADMIN"),
+("User", "user", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "USER")
+
